c13ba96a2b1f16619f747ca2465c54817cfac0c2
Quantum2024 <quantumitconsultancy2024@gmail.com>
2023-11-15 03:18:57 -0400

    Added Conditional Session Checking
    sidebar username on dashboard page doesn't work without it being conditional

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,44 @@
 
+<?php
+
+session_start();
+// Perform the database query to retrieve first_name and last_name based on user_id
+if (isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['user_id'];
+    echo '<script>';
+    echo 'console.log("User ID: ' . $_SESSION['user_id'] . '");';
+    echo '</script>';
+    // Add your database connection code here
+    include "db_connection.php";
+
+    $sql = "SELECT first_name, last_name FROM user WHERE user_id = ?";
+    $stmt = mysqli_prepare($mysqli, $sql);
+
+    // Bind the user_id parameter
+    mysqli_stmt_bind_param($stmt, "i", $userId);
+
+    // Execute the statement
+    mysqli_stmt_execute($stmt);
+
+    // Get the result
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $userName = $row['first_name'] . ' ' . $row['last_name'];
+
+        
+    } else {
+        // Handle the case where user data is not found
+        $userName = "User Not Found";
+    }
+    mysqli_stmt_close($stmt); // Close the prepared statement
+} else {
+    // Handle the case where the user is not logged in
+    $userName = "Guest";
+}
+?>
+
 <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
     <style>
         .nano-content ul li:not(.sidebar-sub-toggle) {
@@ -20,7 +60,7 @@
             <div class="logo" style="background: rgba(255, 255, 255, 0.5);"><a href="index.html">
                     <img src="images/Caribbean_PLL_Logo.png" height="150px" alt="" /> </a></div>
             <div class="text-center">
-                <span class="badge sidebar-badge rounded-pill bg-secondary text-white badge-bigger m-3"><i class="fas fa-user"></i> Eddie Ciraco</span>
+                <span class="badge sidebar-badge rounded-pill bg-secondary text-white badge-bigger m-3"><i class="fas fa-user"></i><?php echo $userName; ?></span>
             </div>
             <ul>
 
