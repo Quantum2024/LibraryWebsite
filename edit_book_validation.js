096a27ff37cbc78bf7ac42dd9bb82dd5d04f6608
Quantum2024 <quantumitconsultancy2024@gmail.com>
2023-11-20 16:43:34 -0400

    Update edit_book_validation.js

--- edit_book_validation.js
+++ edit_book_validation.js
@@ -94,5 +94,126 @@ $(document).ready(function () {
         }
     });
 
+    // New author form validation
+    $("#newAuthorForm").validate({
+        rules: {
+            author_first_name: {
+                required: true,
+                minlength: 2,
+            },
+            author_last_name: {
+                required: true,
+                minlength: 2,
+            },
+        },
+        messages: {
+            author_first_name: {
+                required: "Please enter Author's first name",
+                minlength: "Author's first name must be at least 2 characters",
+            },
+            author_last_name: {
+                required: "Please enter Author's last name",
+                minlength: "Author's last name must be at least 2 characters",
+            },
+        },
+    });
 
+     //Author Modal Validation
+    $("#newAuthorForm").validate({
+        rules: {
+            author_first_name: "required",
+            author_last_name: "required",
+        },
+        messages: {
+            author_first_name: "Please enter Author's first name",
+            author_last_name: "Please enter Author's last name",
+        },
+    });
+
+    // Trigger form validation when the modal is shown
+    $('#NewAuthor').on('shown.bs.modal', function () {
+        // Enable the Save button if the form is valid
+        if ($("#newAuthorForm").valid()) {
+            $('#submitAuthor').prop('disabled', false);
+        } else {
+            $('#submitAuthor').prop('disabled', true);
+        }
+    });
+
+
+   // Validate newPublisherForm
+   $("#newPublisherForm").validate({
+    rules: {
+        publisher_name: {
+            required: true,
+            minlength: 2,
+        },
+        publisher_country: {
+            required: true,
+            minlength: 2,
+        },
+        email_address: {
+            required: true,
+            email: true,
+        },
+        phone_number: {
+            required: true,
+            minlength: 10,
+        },
+    },
+    messages: {
+        publisher_name: {
+            required: "Please enter the publisher's name",
+            minlength: "Publisher's name must be at least 2 characters",
+        },
+        publisher_country: {
+            required: "Please enter the publisher's country",
+            minlength: "Publisher's country must be at least 2 characters",
+        },
+        email_address: {
+            required: "Please enter a valid email address",
+            email: "Please enter a valid email address",
+        },
+        phone_number: {
+            required: "Please enter a valid phone number",
+            minlength: "Phone number must be at least 10 characters",
+        },
+    },
+   
+});
+
+
+$('#NewPublisher').on('shown.bs.modal', function () {
+    if ($("#newPublisherForm").valid()) {
+        $('#submitPublisher').prop('disabled', false);
+    } else {
+        $('#submitPublisher').prop('disabled', 'disabled');
+    }
+});
+
+//Validate Genre Modal
+$("#modalForm").validate({
+    rules: {
+        genre_name: {
+            required: true,
+            minlength: 2,
+        },
+    },
+    messages: {
+        genre_name: {
+            required: "Please enter the genre name",
+            minlength: "Genre name must be at least 2 characters",
+        },
+    },
+    
+});
+
+
+$('#NewGenre').on('shown.bs.modal', function () {
+    if ($("#modalForm").valid()) {
+        $('#submitGenre').prop('disabled', false);
+    } else {
+        $('#submitGenre').prop('disabled', 'disabled');
+    }
+});
 });
