4334f079e9ea7dab6bce5a87f7890a32e76bdcbc
eciraco <149725554+eciraco@users.noreply.github.com>
2023-12-05 14:53:17 -0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
 //  Step 1: Establish a database connection localhost=vm remotehost=xampp stack
-$host = "20.150.222.134:3306";
-//$host = "localhost";
+//$host = "20.150.222.134:3306";
+$host = "localhost";
 $username = "eciraco";
 $password = "eciraco6920";
 $database = "LIS";
@@ -10,10 +10,10 @@ $database = "LIS";
 $mysqli = new mysqli($host, $username, $password, $database);
 
 // Check connection
-if ($mysqli->connect_error) {
-    $message = "Connection failed: " . $connection->connect_error;
+if($mysqli->connect_error) {
+    $message = "Connection failed: ".$connection->connect_error;
 
-    if (isset($_SERVER['HTTP_REFERER'])) {
+    if(isset($_SERVER['HTTP_REFERER'])) {
 
         //$referrer = $_SERVER['HTTP_REFERER'];
         //header("Location: error_page.php?message=$message&return_page=$referrer");
